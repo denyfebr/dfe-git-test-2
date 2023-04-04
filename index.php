00cb989c86f0223a3b9bf2e49874353cb75b81b4
denyfebr <denyfebri@gmail.com>
2023-04-04 07:49:19 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello world this</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis distinctio, harum at, sint saepe in possimus nam quas reprehenderit quasi fugit rerum corrupti ducimus doloremque animi inventore nostrum ullam provident.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
